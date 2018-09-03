51429d5495fe1001e074699c1f74ddb5a0d383e2
Dipen Shah <dipen08it419@gmail.com>
2018-09-03 15:23:47 -0400

    Initial Commit

---
+++ Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace StringFormatTestProject
+{
+    public class TestConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine($"TestConverter: ~{value}~ of type {value?.GetType()} to ~{targetType}~, parameter ~{parameter}~");
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
